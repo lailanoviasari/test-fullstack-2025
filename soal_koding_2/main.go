d73d9bd548bb8cc4d79a57622499dbc8706c8136
lailanoviasari <laila.20013@mhs.unesa.ac.id>
2025-08-27 12:27:47 +0700

    EDIT: edit kode soal 2

--- soal_koding_2/main.go
+++ soal_koding_2/main.go
@@ -1,11 +1,75 @@
-// soal koding 1
+// soal koding 2
 
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+	_"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	// Login Endpoint
+	app.Post("/login", login)
+
+	// Public Endpoint
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("login"),
+	}))
+
+	// Private Route 
+	app.Get("/restricted", restricted)
+
+	app.Listen(":3000")
 }
+
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Lempar unsuthorized status
+	if user != "AbertoDoniSianturi" || pass != "f7c3bc1d808e0" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// membuat claim
+	claims := jwt.MapClaims {
+		"realname" 	: "Aberto Doni Sianturi",
+		"email" 	: "adss@gmail.com",
+		"exp" 		: time.Now().Add(time.Hour*72).Unix(),
+	}
+
+	// generate token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte("login"))
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token" : t})
+
+} 
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Dapat diakses")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	realname := claims["realname"].(string)
+
+	return c.SendString("Welcome " + realname)
+}
+
+
